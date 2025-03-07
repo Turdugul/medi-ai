019812daf8f7a25c1af5b2d589847944fd2c6efc
Turdugul <okonbaevaturdugul@gmail.com>
2025-03-07 23:51:39 +0000

    fixing deployment error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,6 +15,16 @@ const nextConfig = {
       },
     ];
   },
+  // Server configuration
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    hostname: process.env.HOSTNAME || '0.0.0.0',
+  },
+  // Environment variables that will be shared across server and client
+  publicRuntimeConfig: {
+    frontendUrl: process.env.FRONTEND_URL,
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  }
 };
 
 module.exports = nextConfig; 
