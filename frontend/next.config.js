794e7ef22faed45ae9aac5812ae7015af301120f
Turdugul <okonbaevaturdugul@gmail.com>
2025-03-07 23:25:44 +0000

    fix deploy

---
+++ frontend/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
+  output: 'standalone',
+  poweredByHeader: false,
+};
+
+module.exports = nextConfig; 
