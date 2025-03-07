96b1a49ca268298f68b84b73c0295e9c530817f4
Turdugul <okonbaevaturdugul@gmail.com>
2025-03-07 23:43:56 +0000

    fix deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost'],
-  },
+  output: 'standalone',
+  poweredByHeader: false,
   async headers() {
     return [
       {
@@ -16,17 +15,6 @@ const nextConfig = {
       },
     ];
   },
-  output: 'standalone',
-  poweredByHeader: false,
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
-  serverRuntimeConfig: {
-    PORT: process.env.PORT || 3001
-  },
-  env: {
-    PORT: process.env.PORT || 3001
-  }
 };
 
 module.exports = nextConfig; 
