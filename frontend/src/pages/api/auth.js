f2b54edb4f74d2a6e3075ace6149cbcd25c2348f
Turdugul <okonbaevaturdugul@gmail.com>
2025-03-12 05:05:13 +0000

    api fix

--- frontend/src/pages/api/auth.js
+++ frontend/src/pages/api/auth.js
@@ -1,6 +1,6 @@
 
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL 
 export const registerUser = async (userData) => {
   try {
       const response = await fetch(`${API_BASE_URL}/api/auth/register`, {
